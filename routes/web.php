e0106cbc42b1e0673b7891acf0e2779842459327
yosiasp <yosia.535230117@stu.untar.ac.id>
2024-05-31 11:16:14 +0700

    melakukan perubahan pada route untuk mengetes db

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Connected to database: ' . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        return 'Failed to connect to database: ' . $e->getMessage();
+    }
+});
