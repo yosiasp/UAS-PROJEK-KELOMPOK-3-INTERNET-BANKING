51b329d08de95a758a318dca2756a6d2d9afbaf7
drewjohnjaya <167056794+drewjohnjaya@users.noreply.github.com>
2024-06-07 10:35:27 +0700

    remove function in controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,19 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    // function
-    public function index(){
-    }
-    public function create(){
-    }
-    public function store(){
-    }
-    public function show(){
-    }
-    public function edit(){
-    }
-    public function update(){
-    }
-    public function delete(){
-    }
+    //
 }
