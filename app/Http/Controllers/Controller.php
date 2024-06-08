d90ac1e86b1c4000f2e764f3c92513e578f54573
FelixFerdinad <154125390+FelixFerdinad@users.noreply.github.com>
2024-06-08 23:49:18 +0700

    Update Controller.php
    membuat 3 fungsi :
    1. showRegistrationForm fungsi untuk menampilkan halam form pendaftaran
    2. resgister fungsi untuk mengirim form pendaftaran 
    3. showTermsAndConditions fungsi untuk menampilkan halaman form yang harus disetujui

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
-abstract class Controller
+class UserControllwe extends Controller
 {
-    //
+    // Fungsi untuk menampilkan form pendaftaran (boleh ubah variabelnya biar sama)
+    public function showRegistrationForm()
+    {
+        return view('register');
+}
+    // Fungsi untuk mengirim form pendaftaran (boleh ubah variabelnya biar sama)
+    public function register(Request $request)
+    {
+        
+    }
+
+    // Fungsi untuk menampilkan syarat dan ketentuan ((boleh ubah variabelnya biar sama)
+    public function showTermsAnd Conditions()
+    {
+        return view('terms')
+    }
 }
