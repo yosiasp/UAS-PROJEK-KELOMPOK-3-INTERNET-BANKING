0b075ada46a2bdea323cf5c38b010736ad2c90db
FelixFerdinad <154125390+FelixFerdinad@users.noreply.github.com>
2024-06-17 17:29:53 +0700

    menambahkan route untuk ChangeEmail

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TransferController;
 use App\Http\Controllers\MutationController;
 use App\Http\Controllers\BalanceInfoController;
+use App\Htt\Controllers|UpdateEmailController;
 
 Route::get('/customer-service', function () {
     return view('customerService');
@@ -36,3 +37,7 @@ Route::get('/{id}/home/change-pin', [PinController::class, 'index'])->name('chan
 Route::post('/{id}/home/change-pin', [PinController::class, 'update'])->name('change-pin');
 
 Route::get('/{id}/balanceInfo', [BalanceInfoController::class, 'index'])->name('balanceInfo');
+
+Route::('/change-email', [EditEmailController::class, 'showChangeEmailForm'])->name('changeEmailForm);
+Route::post('change-email', [EditEmailController::class, 'updateEmail')]->name('updateEmail');
+                                                                                    
