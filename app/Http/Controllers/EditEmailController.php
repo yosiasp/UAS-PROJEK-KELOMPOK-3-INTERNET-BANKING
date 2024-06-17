cb5dd3b119119086a64a360602c64f2a5eb0e7bb
FelixFerdinad <154125390+FelixFerdinad@users.noreply.github.com>
2024-06-17 16:13:52 +0700

    perbaiki typo #2 EditEmailController.php

--- app/Http/Controllers/EditEmailController.php
+++ app/Http/Controllers/EditEmailController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-Use Illumintate\Support\Facades\Hash;
+use Illumintate\Support\Facades\Hash;
 use App\Models\User;
 
 class EditEmailController extends Controller
