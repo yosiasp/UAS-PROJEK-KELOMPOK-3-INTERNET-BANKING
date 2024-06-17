38a2570772b1b5e9558ad2d721e2e61a16c0dc40
Valentino812 <untarvalen123@gmail.com>
2024-06-17 14:20:25 +0700

    new page for balance info

---
+++ app/Http/Controllers/BalanceInfoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use App\Models\Balance;
+use Illuminate\Http\Request;
+
+class BalanceInfoController extends Controller
+{
+    public function index($id) 
+    {
+        $account = Account::find($id);
+        
+  
+            $username = $account->username;
+            $balanceInfo = Balance::where('username', $username)->first();
+            
+            return view("balanceInfo", compact('account', 'balanceInfo'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PinController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TransferController;
 use App\Http\Controllers\MutationController;
+use App\Http\Controllers\BalanceInfoController;
 
 Route::get('/customer-service', function () {
     return view('customerService');
@@ -33,3 +34,5 @@ Route::get('/{id}/mutation', [MutationController::class, 'index'])->name('mutati
 
 Route::get('/{id}/home/change-pin', [PinController::class, 'index'])->name('changePin');
 Route::post('/{id}/home/change-pin', [PinController::class, 'update'])->name('change-pin');
+
+Route::get('/{id}/balanceInfo', [BalanceInfoController::class, 'index'])->name('balanceInfo');
