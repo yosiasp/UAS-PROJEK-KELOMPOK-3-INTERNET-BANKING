1901d34d416b6c7c845ddcf9523ad9c3001b67f7
Valentino812 <untarvalen123@gmail.com>
2024-06-18 17:54:15 +0700

    web php fix

--- routes/web.php
+++ routes/web.php
@@ -40,4 +40,4 @@ Route::get('/{id}/balanceInfo', [BalanceInfoController::class, 'index'])->name('
 Route::get('/{id}/change-phone', [ChangePhoneController::class, 'index'])->name('changePhone');
 Route::patch('/{id}/change-phone', [ChangePhoneController::class, 'updatePhone'])->name('change-phone');
 
-Route::post('/logout', LogOutController::class)->name('logout');
+// Route::post('/logout', LogOutController::class)->name('logout');
