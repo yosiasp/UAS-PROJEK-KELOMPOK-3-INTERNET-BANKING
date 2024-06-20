1f7734312e16f0e72a6eacc4234ff01f133f9d05
Valentino812 <untarvalen123@gmail.com>
2024-06-20 16:31:31 +0700

    changing titles

--- resources/views/changeEmail.blade.php
+++ resources/views/changeEmail.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/changeEmail.css') }}">
-    <title>Ganti Alamat Email</title>
+    <title>Ubah Alamat Email</title>
 </head>
 <body>
     <div class="header">
