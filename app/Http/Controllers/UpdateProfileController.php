09e252c750fcf453a4c0a7595df82b5a15c3f143
Valentino812 <untarvalen123@gmail.com>
2024-06-21 14:21:15 +0700

    updateProfile page fix

---
+++ app/Http/Controllers/UpdateProfileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Account;   
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UpdateProfileController extends Controller
+{
+    public function index($id)
+    {
+        $account = Account::find($id);
+
+        return view("updateProfile",  compact('account'));
+    }
+
+    public function updateProfile(Request $request, $id)
+    {
+        // Validasi 
+        $validator = Validator::make($request->all(), [
+            'fullname' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z\s]+$/'],
+            'dob' => 'required|date',
+            'gender' => 'required|string|in:Laki-laki,Perempuan',
+            'address' => 'required|string|max:255',
+        ]);
+
+        // Jika validasi gagal
+        if ($validator->fails()) {
+            return redirect()->route('updateProfile', ['id' => $id])->with('error', 'Pembaruan data diri gagal, pastikan data diri yang dimasukan valid valid');
+        } else {
+            $account = Account::find($id);
+
+            if($account->fullname != $request->fullname){
+                $account->fullname = $request->fullname;
+            }
+            
+            if($account->dob != $request->dob){
+                $account->dob = $request->dob;
+            }
+
+            if($account->fullname != $request->fullname){
+                $account->fullname = $request->fullname;
+            }
+            
+            if($account->gender != $request->gender){
+                $account->gender = $request->gender;
+            }
+
+            if($account->address != $request->address){
+                $account->address = $request->address;
+            }
+
+            if($account->address != $request->address){
+                $account->address = $request->address;
+            }
+                 
+            $account->save();
+            
+            return redirect()->route('updateProfile', ['id' => $id])->with('success', 'Pembaruan data diri berhasil');
+        }
+
+    }
+}
+
+        
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\ChangePhoneController;
 use App\Http\Controllers\LogInController;
 use App\Http\Controllers\LogOutController;
 use App\Http\Controllers\ChangeEmailController;
-use App\Http\Controllers\EditProfileController; //tambahan controllers
+use App\Http\Controllers\UpdateProfileController; 
 
 Route::get('/', [MainController::class, 'index'])->name('main');
 
@@ -50,9 +50,9 @@ Route::middleware(['guest'])->group(
 
         Route::get('/{id}/change-email', [ChangeEmailController::class, 'index'])->name('changeEmail');
         Route::patch('/{id}/change-email', [ChangeEmailController::class, 'updateEmail'])->name('change-email');
-        //tambahan route editprofile
-        Route::get('/{id}/edit-profile', [EditProfileController::class, 'editProfilePage'])->name('editProfilePage');
-        Route::post('/{id}/update-profile', [EditProfileController::class, 'updateProfile'])->name('updateProfile');
+        
+        Route::get('/{id}/update-profile', [UpdateProfileController::class, 'index'])->name('updateProfile');
+        Route::post('/{id}/update-profile', [UpdateProfileController::class, 'updateProfile'])->name('update-profile');
 
         Route::post('/logout', LogOutController::class)->name('logout');
     }
